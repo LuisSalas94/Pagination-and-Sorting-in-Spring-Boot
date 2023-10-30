2861619e284bec65c7048d509bab448b7a69504b
Fernando Salas <luisfernandosalasg@gmail.com>
2023-10-30 15:16:18 -0500

    Config MySql database

---
+++ src/main/java/net/fernandosalas/posts/PostsDemoApplication.java
@@ -0,0 +1,13 @@
+package net.fernandosalas.posts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/fernandosalas/posts/PostsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.fernandosalas.posts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
